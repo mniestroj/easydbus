caea308a38d53f41247118527bc6d4390513f685
Marcin Niestroj <m.niestroj@grinn-global.com>
2017-05-09 15:56:41 +0200

    spec: Test lua-ev returning DBus errors

--- spec/loop_ev_spec.lua
+++ spec/loop_ev_spec.lua
@@ -17,6 +17,16 @@ local loop = ev.Loop.default
 
 loop_ev.wrap(loop)
 
+local function add_callback(f)
+   local coro = coroutine.create(f)
+   local idle
+   idle = ev.Idle.new(function()
+         coroutine.resume(coro)
+         idle:stop(loop)
+   end)
+   idle:start(loop)
+end
+
 describe('Service creation', function()
    it('Own and unown name', function()
       local bus = assert(dbus[bus_name]())
@@ -42,16 +52,6 @@ describe('Service creation', function()
       local dummy_handler = spy.new(function() end)
       object:add_method('dummy', '', '', dummy_handler)
 
-      local function add_callback(f)
-         local coro = coroutine.create(f)
-         local idle
-         idle = ev.Idle.new(function()
-            coroutine.resume(coro)
-            idle:stop(loop)
-         end)
-         idle:start(loop)
-      end
-
       local ret
       add_callback(function()
          ret = pack(bus:call(service_name, object_path, interface_name, 'dummy'))
@@ -63,5 +63,76 @@ describe('Service creation', function()
 
       assert.spy(dummy_handler).was.called()
       assert.are.same(pack(), ret)
+   end)
+end)
+
+describe('Returning DBus errors', function()
+   local bus, object
+   local g_error = error
+   local error = function(msg)
+      return g_error(msg, 0)
+   end
+
+   before_each(function()
+         bus = assert(dbus[bus_name]())
+         assert(bus:own_name(service_name))
+         object = bus:object(object_path, interface_name)
+         object:add_method('Empty', '', '', function() end)
+   end)
+
+   after_each(function()
+         assert(bus:unown_name(service_name))
+   end)
+
+   it('Simple', function()
+      object:add_method('Error1', '', '', function()
+         error('Error1')
+      end)
+
+      local ret
+      add_callback(function()
+         ret = pack(bus:call(service_name, object_path, interface_name, 'Error1'))
+         loop:unloop()
+      end)
+      loop:loop()
+
+      assert.are.same(pack(nil, 'org.freedesktop.DBus.Error.Failed', 'Error1'), ret)
+   end)
+
+   it('Single yield in handler', function()
+      object:add_method('Error2', '', '', function()
+         -- Yield by calling an empty method from ourselves
+         bus:call(service_name, object_path, interface_name, 'Empty')
+         error('Error2')
+      end)
+
+      local ret
+      add_callback(function()
+         ret = pack(bus:call(service_name, object_path, interface_name, 'Error2'))
+         loop:unloop()
+      end)
+      loop:loop();
+
+      assert.are.same(pack(nil, 'org.freedesktop.DBus.Error.Failed', 'Error2'), ret)
+   end)
+
+   it('Multiple yields in handler', function()
+      object:add_method('Error3', '', '', function()
+         -- Yield by calling multiple empty methods from ourselves
+         bus:call(service_name, object_path, interface_name, 'Empty')
+         bus:call(service_name, object_path, interface_name, 'Empty')
+         bus:call(service_name, object_path, interface_name, 'Empty')
+         bus:call(service_name, object_path, interface_name, 'Empty')
+         error('Error3')
+      end)
+
+      local ret
+      add_callback(function()
+         ret = pack(bus:call(service_name, object_path, interface_name, 'Error3'))
+         loop:unloop()
+      end)
+      loop:loop();
+
+      assert.are.same(pack(nil, 'org.freedesktop.DBus.Error.Failed', 'Error3'), ret)
    end)
 end)
